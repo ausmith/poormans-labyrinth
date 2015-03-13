d0376a31356f3b95a13d9592fce859ba6c2cc45e
ausmith <aaron.goldsmith@careerbuilder.com>
2015-03-13 09:41:36 -0400

    Show more interesting results

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,5 +5,7 @@ class IndexController < ApplicationController
     @num_products = Product.count
     @num_products_ordered = ProductOrder.count
     @answers = Answer.all
+    order_totals = Order.all.map { |o| o.products.map(&:price).sum }
+    @avg_order = (order_totals.sum.to_f / order_totals.size).round(2)
   end
 end
